e96e076da525cfbac8f027ed36c0c35a62c2d70c
jandrei <jandrei.cm@gmail.com>
2014-09-10 18:15:08 -0300

    criação webservices novos.

--- findbusweb/src/main/java/findbusweb/Rest.java
+++ findbusweb/src/main/java/findbusweb/Rest.java
@@ -10,19 +10,42 @@ import com.google.gson.Gson;
 
 import findbusweb.agency.AgencyService;
 import findbusweb.ed.Agency;
+import findbusweb.ed.Routes;
+import findbusweb.ed.Stops;
+import findbusweb.routes.RouteService;
+import findbusweb.stops.StopService;
 
-@Path("/teste")
+@Path("/r")
 @ManagedBean
 public class Rest {
 	
 	@Inject
 	AgencyService agencyService;
 	
+	@Inject
+	RouteService routeService;
+
+	@Inject
+	StopService stopService;
 	
 	@GET
-	@Path("/teste")
-	public Response teste(){
+	@Path("/agencys")
+	public Response agencys(){
 		java.util.List<Agency> agencias = agencyService.listar(new Agency());
 		return Response.ok(new Gson().toJson(agencias)).build();
+	}
+
+	@GET
+	@Path("/routes")
+	public Response routes(){
+		java.util.List<Routes> rotas = routeService.listar(new Routes());
+		return Response.ok(new Gson().toJson(rotas)).build();
+	}
+	
+	@GET
+	@Path("/stops")
+	public Response stops(){
+		java.util.List<Stops> rotas = stopService.listar(new Stops());
+		return Response.ok(new Gson().toJson(rotas)).build();
 	}
 }
